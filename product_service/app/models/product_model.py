bf8f3b7c3b8fc672d7cd169ae1a68cd1fa45f15a
muhammadammar24 <muhammadammar2492@gmail.com>
2024-07-29 17:14:03 +0500

    todo proto

--- product_service/app/models/product_model.py
+++ product_service/app/models/product_model.py
@@ -2,17 +2,20 @@ from sqlmodel import SQLModel, Field
 
 
 class Product(SQLModel, table=True):
-    id : int | None = Field(default=None, primary_key=True)
-    title : str
-    description: str
-    category : str
-    price : float
-    discount : int | None = None
-    quantity : float 
-    brand : str | None = None
-    weight : float | None 
-    expiry : str | None = None
-    sku : str | None = None
+    # id : int | None = Field(default=None, primary_key=True)
+    # title : str
+    # description: str
+    # category : str
+    # price : float
+    # discount : int | None = None
+    # quantity : float 
+    # brand : str | None = None
+    # weight : float | None 
+    # expiry : str | None = None
+    # sku : str | None = None
+    
+    id: Optional[int] = Field(default=None, primary_key=True)
+    content: str = Field(index=True)
     
 
 # Update Product
